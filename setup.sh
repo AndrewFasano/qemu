ad68a5f7e6d9e95f9099e1395418c1b4a014b905
Andrew Fasano <fasano@mit.edu>
2023-01-30 11:33:56 -0500

    Add qemu_setup helper

---
+++ setup.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+# Usage: ./qemu_setup.sh [build_dir] [target-list] [extra_config_args...]
+
+set -eux
+mkdir -p "/root/libafl_qemu/$1"; 
+cd "/root/libafl_qemu/$1";
+shift
+
+TARGETS="$1"
+shift
+/root/libafl_qemu/configure \
+                --target-list=${TARGETS} \
+                --enable-system \
+                --disable-slirp \
+                --enable-plugins \
+                --enable-fdt=internal \
+                --audio-drv-list= \
+                --disable-alsa \
+                --disable-attr \
+                --disable-auth-pam \
+                --disable-dbus-display \
+                --disable-bochs \
+                --disable-bpf \
+                --disable-brlapi \
+                --disable-bsd-user \
+                --disable-bzip2 \
+                --disable-cap-ng \
+                --disable-canokey \
+                --disable-cloop \
+                --disable-cocoa \
+                --disable-coreaudio \
+                --disable-curl \
+                --disable-curses \
+                --disable-dmg \
+                --disable-docs \
+                --disable-dsound \
+                --disable-fuse \
+                --disable-fuse-lseek \
+                --disable-gcrypt \
+                --disable-gettext \
+                --disable-gio \
+                --disable-glusterfs \
+                --disable-gnutls \
+                --disable-gtk \
+                --disable-guest-agent \
+                --disable-guest-agent-msi \
+                --disable-hax \
+                --disable-hvf \
+                --disable-iconv \
+                --disable-jack \
+                --disable-keyring \
+                --disable-kvm \
+                --disable-libdaxctl \
+                --disable-libiscsi \
+                --disable-libnfs \
+                --disable-libpmem \
+                --disable-libssh \
+                --disable-libudev \
+                --disable-libusb \
+                --disable-linux-aio \
+                --disable-linux-io-uring \
+                --disable-linux-user \
+                --disable-live-block-migration \
+                --disable-lzfse \
+                --disable-lzo \
+                --disable-l2tpv3 \
+                --disable-malloc-trim \
+                --disable-mpath \
+                --disable-multiprocess \
+                --disable-netmap \
+                --disable-nettle \
+                --disable-numa \
+                --disable-nvmm \
+                --disable-opengl \
+                --disable-oss \
+                --disable-pa \
+                --disable-parallels \
+                --disable-png \
+                --disable-pvrdma \
+                --disable-qcow1 \
+                --disable-qed \
+                --disable-qga-vss \
+                --disable-rbd \
+                --disable-rdma \
+                --disable-replication \
+                --disable-sdl \
+                --disable-sdl-image \
+                --disable-seccomp \
+                --disable-selinux \
+                --disable-slirp-smbd \
+                --disable-smartcard \
+                --disable-snappy \
+                --disable-sparse \
+                --disable-spice \
+                --disable-spice-protocol \
+                --disable-tools \
+                --disable-tpm \
+                --disable-usb-redir \
+                --disable-user \
+                --disable-u2f \
+                --disable-vde \
+                --disable-vdi \
+                --disable-vduse-blk-export \
+                --disable-vhost-crypto \
+                --disable-vhost-kernel \
+                --disable-vhost-net \
+                --disable-vhost-user-blk-server \
+                --disable-vhost-vdpa \
+                --disable-virglrenderer \
+                --disable-virtfs \
+                --disable-virtiofsd \
+                --disable-vmnet \
+                --disable-vnc \
+                --disable-vnc-jpeg \
+                --disable-vnc-sasl \
+                --disable-vte \
+                --disable-vvfat \
+                --disable-whpx \
+                --disable-xen \
+                --disable-xen-pci-passthrough \
+                --disable-xkbcommon \
+                --disable-zstd \
+                --disable-capstone \
+                --disable-sndio \
+                $@
+    make -j
